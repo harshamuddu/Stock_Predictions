b0cb6dd23df2d71e1c3a68b1f144b7ba25a3dd75
Shawn Sanderlin <shawn.sanderlin@gmail.com>
2020-02-09 02:30:45 -0500

    Create py_low_sell_high.py

---
+++ py_low_sell_high.py
@@ -0,0 +1,19 @@
+#Need a short moving average
+
+#Need a mid moving average
+
+#Need a long moving average
+
+#buys when its below a var % of mid moving average and sells when its above 
+
+#buys when previous week had a negitive slope and current week has a flat slope
+
+#sells when previous week had a positive slope and current week has a flating slope
+
+#rates stocks on there volatility
+
+#based off a stocks volatility we will choose to use the short mid or long moving average
+#we can make more off of higher vol stocks but also can lose alot more
+
+
+
